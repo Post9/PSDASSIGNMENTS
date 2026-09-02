cb89d2aa57bc8f2477a3c2aa513d78e27b81a7cb
Post9 <nielslaierjensen@gmail.com>
2026-09-02 11:55:08 +0200

    1.1 1st attempt

--- Assignments/assignment1/Intro/Intro2.fs
+++ Assignments/assignment1/Intro/Intro2.fs
@@ -33,6 +33,7 @@ let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 
 
 (* Evaluation within an environment *)
+// Missing type check no f# 
 
 let rec eval e (env : (string * int) list) : int =
     match e with
@@ -41,9 +42,30 @@ let rec eval e (env : (string * int) list) : int =
     | Prim("+", e1, e2) -> eval e1 env + eval e2 env
     | Prim("*", e1, e2) -> eval e1 env * eval e2 env
     | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim _            -> failwith "unknown primitive";;
+
+        // max min and equals
+
+    | Prim("max", e1, e2) -> 
+        if eval e1 env >= eval e2 env then
+            eval e1 env   x
+        else 
+            eval e2 env 
+    | Prim("min", e1, e2) ->
+        if eval e1 env <= eval e2 env then
+            eval e1 env else
+            eval e2 env
+    | Prim("==", e1, e2) -> 
+        if eval e1 env = eval e2 env then 1 else 0
+    | Prim _            -> failwith "unknown primitive";;   
+    
+    
+    
+// 1.2 
+let e3 = Prim("max", Prim("*", Var "b", CstI 9), Var "a");;
+
 
 let e1v  = eval e1 env;;
 let e2v1 = eval e2 env;;
 let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
+
